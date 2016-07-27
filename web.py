fddd5e7849bfb6e172ffe075e2d86ea9ec203f1b
Tiago Ilieve <tiago.myhro@gmail.com>
2016-07-27 11:20:21 -0300

    Initial commit

---
+++ web.py
@@ -0,0 +1,11 @@
+import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return str(datetime.datetime.now())
+
+if __name__ == '__main__':
+    app.run()
